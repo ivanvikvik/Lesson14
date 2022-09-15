13cc86457e68335256334c4bf1e7846571d473c0
vikvik <ivanvikvik@gmail.com>
2022-09-15 10:34:53 +0300

    Did refactoring of Task06 with static method

--- src/Task06.java
+++ src/Task06.java
@@ -1,14 +1,23 @@
-import java.util.Scanner;
-
 public class Task06 {
-    public static void main(String[] args) {
-        int n = 10;
 
-        for (int j = 1; j < n; j++) {
-            for (int i = 1; i < n; i++) {
-                System.out.print(i * j + "\t");
+    public static String getPythagorasTable(int dimension){
+        String msg = "";
+
+        for (int j = 1; j <= dimension; j++) {
+            for (int i = 1; i <= dimension; i++) {
+                msg += i * j + "\t";
             }
-            System.out.println();
+            msg += "\n";
         }
+
+        return msg;
+    }
+
+    public static void main(String[] args) {
+        int dimension = Task02.inputPositiveNumber();
+
+        String result = getPythagorasTable(dimension);
+
+        System.out.println(result);
     }
 }
